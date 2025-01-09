168d8dfb0238c16f82535f01f638eef42dc37fc7
ubuntu <ubuntu@ip-10-0-1-25>
2025-01-09 23:54:07 +0000

    waleko__gpt-4o_7e6b__id_116

--- wandb/integration/ultralytics/bbox_utils.py
+++ wandb/integration/ultralytics/bbox_utils.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict, List, Optional, Tuple, Union
 
-import torch
-from ultralytics.engine.results import Results
+import torchfrom ultralytics.engine.results import Results
 from ultralytics.models.yolo.detect import DetectionPredictor
 try:
     from ultralytics.yolo.utils import ops
