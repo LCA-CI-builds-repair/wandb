68e269adc6ac838d8e67cac134d9ba10cbabcd73
ubuntu <ubuntu@ip-10-0-1-60>
2025-01-12 18:35:42 +0000

    waleko__claude-3-opus_ef67__id_116

--- wandb/integration/ultralytics/bbox_utils.py
+++ wandb/integration/ultralytics/bbox_utils.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import torch
 from ultralytics.engine.results import Results
